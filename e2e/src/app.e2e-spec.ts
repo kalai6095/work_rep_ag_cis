4d503c30c3128de52c56f5e7242b1c902d64cde0
Kalaiselvan A <kalai6095@gmail.com>
2019-07-02 05:53:01 +0000

    protractor page code commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,8 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to work-rep!');
+    console.log(page.getTitleText());
+    expect(page.getTitleText()).toEqual('Work Reports');
   });
 
   afterEach(async () => {
